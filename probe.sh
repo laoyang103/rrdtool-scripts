26a72be19f4885f04d6fd6da01b38842930c972f
laoyang103 <553069938@qq.com>
2015-07-04 15:24:26 +0800

    add cpu statistics

--- probe.sh
+++ probe.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 pid_list=2147483647
-proc_list="java mysqld ipmtube ipms"
+proc_list="java mysqld ipmtube ipms tcpreplay rrdcached"
 output_times=100
 
 for proc in $proc_list; do
-    pids=`ps -e | grep $proc | awk '{ print $1 }' | xargs | sed 's/ /,/g'`
-    if [ $pids ]; then
-        pid_list=$pid_list","$pids
+    pid=`ps -e | grep $proc | sed -n 1,1p | awk '{ print $1 }'`
+    if [ $pid ]; then
+        pid_list=$pid_list","$pid
     fi
 done
 
